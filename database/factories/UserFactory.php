298b07682609d3f06770c2e2e8ee80dd147b7633
kasse222 <kasselamine130@gmail.com>
2025-06-09 13:24:22 +0100

    feat(factory): generate realistic users with role (expediteur, voyageur)

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,7 +29,12 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'role' => fake()->randomElement(['expediteur', 'voyageur']), // admin non généré automatiquement
         ];
+    }
+    public function admin(): static
+    {
+        return $this->state(fn() => ['role' => 'admin']);
     }
 
     /**
@@ -37,7 +42,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
